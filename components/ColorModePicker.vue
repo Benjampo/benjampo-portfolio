cb64a238b6b61f638ea3ed2d90d0a79fcf648812
Benjamin Porchet <benjaminporchet.pro@gmail.com>
2022-04-30 20:22:58 +0200

    update

--- components/ColorModePicker.vue
+++ components/ColorModePicker.vue
@@ -1,13 +1,11 @@
 <template>
-  <div>
-    <input v-model="$colorMode.preference" name="color" value="dark" type="radio">
-    <input v-model="$colorMode.preference" name="color" value="light" type="radio">
-    <component :is="`icon-${$colorMode.preference}`" />
+  <div class="color-mode">
+    <component :is="`icon-${$colorMode.preference}`" @click="toggleColor()" />
   </div>
 </template>
 <script>
-import IconLight from 'static/icons/light.svg?inline'
-import IconDark from 'static/icons/dark.svg?inline'
+import IconLight from 'static/icons/dark.svg?inline'
+import IconDark from 'static/icons/light.svg?inline'
 
 export default {
   components: {
@@ -15,45 +13,14 @@ export default {
     IconDark
   },
   props: ['selected'],
-  data () {
-    return {
-      colors: ['light', 'dark']
-    }
-  },
   methods: {
-    getClasses (color) {
-      // Does not set classes on ssr when preference is system (because we don't know the preference until client-side)
-      if (this.$colorMode.unknown) {
-        return {}
-      }
-      return {
-        preferred: color === this.$colorMode.preference,
-        selected: color === this.$colorMode.value
+    toggleColor () {
+      if (this.$colorMode.preference === 'light') {
+        this.$colorMode.preference = 'dark'
+      } else {
+        this.$colorMode.preference = 'light'
       }
     }
   }
 }
 </script>
-<style scoped>
-.feather {
-  position: relative;
-  top: 0px;
-  cursor: pointer;
-  padding: 7px;
-  background-color: var(--bg-secondary);
-  border: 2px solid var(--border-color);
-  margin: 0;
-  border-radius: 5px;
-  transition: all 0.1s ease;
-}
-.feather:hover {
-  top: -3px;
-}
-.feather.preferred {
-  border-color: var(--color-primary);
-  top: -3px;
-}
-.feather.selected {
-  color: var(--color-primary);
-}
-</style>
