127dc108ef0c1310ae5db946343568a926c87839
Benjamin Porchet <benjaminporchet.pro@gmail.com>
2022-05-01 15:36:47 +0200

    update transitiont est

--- components/Icon.vue
+++ components/Icon.vue
@@ -1,18 +1,13 @@
 <template>
-  <img class="icon" :src="getImgUrl" :alt="icon">
+  <img :key="$colorMode.preference" class="icon" :src="getImgUrl" :alt="pic">
 </template>
 <script>
 export default {
   name: 'IconSvg',
-  props: ['pic'],
-  data () {
-    return {
-      icon: '',
-      rellax: {
-        speed: 3
-      }
-    }
+  key (route) {
+    return route.fullPath
   },
+  props: ['pic'],
   computed: {
     getImgUrl () {
       return require('./../static/3d_assets/' + this.$colorMode.preference + '/' + this.pic + '.svg')
--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,68 @@
 <template>
-  <main>
-    <section class="headline">
-      <div class="wrapper">
-        <div class="headline__icons">
-          <Icon pic="coffee" :rellax="{speed: -5}" />
-          <Icon pic="light" :rellax="{speed: -5}" />
-          <Icon pic="megaphone" :rellax="{speed: -5}" />
+  <main class="home">
+    <div class="wrapper">
+      <section class="headline">
+        <div class="wrapper">
+          <div class="headline__icons">
+            <transition name="fade">
+              <Icon v-rellax="{ speed: -3}" pic="coffee" />
+            </transition>
+              <Icon v-rellax="{ speed: 5}" pic="light" />
+              <Icon v-rellax="{ speed: -1}" pic="megaphone" />
+          </div>
+          <div v-rellax="rellax" class="headline__content">
+            <span>{{ document.data.headline }}</span>
+            <h1> {{ document.data.pageTitle }} </h1>
+            <span>{{ document.data.Subline }}</span>
+          </div>
         </div>
-        <div class="headline__content">
-          <span>{{ document.data.headline }}</span>
-          <h1>{{ document.data.pageTitle }}</h1>
-          <span>{{ document.data.Subline }}</span>
-        </div>
-      </div>
-    </section>
-    <section class="projects">
-      <div class="wrapper">
-        <ul>
-          <li v-for="project in projects.results" :key="project.uid">
-            <NuxtLink :to="project.uid">
-              <span> {{ project.data.title }} </span>
-              <PrismicImage :field="project.data.image" />
-            </NuxtLink>
-          </li>
-        </ul>
-      </div>
-    </section>
-    <section class="partners">
-      <div class="wrapper">
-        <Icon pic="pin" :rellax="{speed: -5}" />
-        <h2> {{ document.data.trustTitle }} </h2>
-        <div class="partners__container">
+      </section>
+      <section class="projects">
+        <div class="wrapper">
           <ul>
-            <li v-for="partner in document.data.partners" :key="partner.id">
-              <PrismicLink target="_blank" :field="partner.website">
-                <PrismicImage :field="partner.image" />
-              </PrismicLink>
+            <li v-for="project in projects.results" :key="project.uid">
+              <NuxtLink :to="project.uid">
+                <span > {{ project.data.title }} </span>
+                <PrismicImage :field="project.data.image" />
+              </NuxtLink>
             </li>
           </ul>
         </div>
+      </section>
+      <section class="partners">
+        <div class="wrapper">
+          <Icon pic="pin" />
+          <h2> {{ document.data.trustTitle }} </h2>
+          <div class="partners__container">
+            <ul>
+              <li  v-for="partner in document.data.partners" :key="partner.id">
+                <PrismicLink target="_blank" :field="partner.website">
+                  <PrismicImage :field="partner.image" />
+                </PrismicLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <div class="button__container">
+        <NuxtLink class="button" to="/contact">Me contacter</NuxtLink>
       </div>
-    </section>
-    <div class="button__container">
-      <NuxtLink class="button" to="/contact">Me contacter</NuxtLink>
     </div>
   </main>
 </template>
 <script>
 import Icon from '@/components/Icon'
+
 export default {
   name: 'IndexMain',
   components: { Icon },
-
+  data () {
+    return {
+      rellax: {
+        speed: 2
+      }
+    }
+  },
   async asyncData ({ $prismic, params, error }) {
     const document = await $prismic.api.getSingle('hompage')
     const projects = await $prismic.api.query(
@@ -70,3 +81,15 @@ export default {
 
 }
 </script>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: transform 2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+
+  transform: translate(300);
+}
+</style>
